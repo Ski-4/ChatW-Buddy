aba25bf327a954e177c593d8930051cbc827f4e8
Pushpak <pushpakkamboj@gmail.com>
2022-09-22 19:11:20 +0530

    Website Deployed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const io = new Server(server);
 
 app.use(express.static("public"));
